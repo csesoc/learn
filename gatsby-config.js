3ee2d701d94a7da6426f1d5897729bb1c0adae84
Gordon Huang <dqna64@gmail.com>
2022-02-26 00:00:02 +1100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
