2204c8b3daecd7edbe6b8c6bedfe67c11e0243c4
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2022-03-05 21:29:48 +1100

    Add types to publications

--- src/pages/publications.tsx
+++ src/pages/publications.tsx
@@ -1,41 +1,61 @@
-import React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import * as React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
 
-interface propTypes {
-  data: any;
-}
+type Node = {
+    frontmatter: {
+        date: Date;
+        title: string;
+    };
+    slug: string;
+    id: string;
+};
 
-const Publications = (props: propTypes) => {
+type PropTypes = {
+    data: {
+        allMdx: {
+            nodes: Node[];
+        };
+    };
+};
 
-  return (
-    <Layout pageTitle="Publications">
-      <p>Article cards go here</p>
-      <ul>
-        {props.data.allFile.nodes
-          .filter((node: any) => node.extension === 'mdx')
-          .map((node: any) => (
-            <li key={node.id}>{node.name}</li>
-          ))}
-      </ul>
-    </Layout>
-  )
-}
+const Publications = ({ data }: PropTypes) => {
+    return (
+        <Layout pageTitle="My Blog Posts">
+            {data &&
+                data.allMdx &&
+                data.allMdx.nodes &&
+                data.allMdx.nodes.map((node: Node) => (
+                    <article key={node.id}>
+                        <h2>
+                            <Link to={`/blog/${node.slug}`}>
+                                {node.frontmatter.title}
+                            </Link>
+                        </h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                    </article>
+                ))}
+        </Layout>
+    );
+};
 
 /*
  * Pages make graphql queries using "page queries" like so.
  * This queried data is fed into the page component on initial render.
  */
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        id
-        extension
-        name
-      }
+    query {
+        allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+            nodes {
+                frontmatter {
+                    date(formatString: "MMMM D, YYYY")
+                    title
+                }
+                id
+                slug
+            }
+        }
     }
-  }
-`
+`;
 
-export default Publications
+export default Publications;
