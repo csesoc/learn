bb64ad5e7e18a696ace1c4a1da4a6575ae3b458c
Max Phillips <max.phillips@nine.com.au>
2022-03-27 23:19:34 +1100

    Ok it looks good now

---
+++ web/components/Logo.tsx
@@ -0,0 +1,8 @@
+import { Terminal } from "phosphor-react";
+
+export const Logo = () => (
+  <div className="flex items-center space-x-1">
+    <Terminal size="1.5rem" weight="bold" />
+    <span className="text-xl">learn.csesoc</span>
+  </div>
+);
