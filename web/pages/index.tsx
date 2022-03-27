ddf8cb05aa3e3fab76f37ef1143c88faf5ca9ee2
Max Phillips <max.phillips@nine.com.au>
2022-03-27 23:47:30 +1100

    Add a cute 3d model

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,10 +1,26 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import type { NextPage } from "next";
+import { Suspense } from "react";
+import Model from "../components/Scene";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-blackA-12 h-screen w-full">
-      <main className="text-whiteA-12 flex h-full py-40 px-20">
+      <main className="text-whiteA-12 flex h-full flex-col items-start py-40 px-20">
         <h1 className="text-6xl">Level up your dev skills.</h1>
+        <Canvas className="">
+          <OrbitControls />
+          <Suspense fallback={<p>Loading...</p>}>
+            <Model scale={1} />
+          </Suspense>
+        </Canvas>
+        <a
+          className="text-whiteA-12 font-light italic"
+          href="https://sketchfab.com/3d-models/smol-ame-in-an-upcycled-terrarium-hololiveen-490cecc249d242188fda5ad3160a4b24"
+        >
+          This lovely model was made by Seaform on Sketchfab.
+        </a>
       </main>
     </div>
   );
