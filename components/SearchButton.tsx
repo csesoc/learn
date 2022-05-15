0e13a6fd0776ca4b4ab65fbe7286345250a672e7
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-05-15 21:46:32 +1000

    Completed SearchOverlay

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -1,22 +1,19 @@
-import { IconButton } from './IconButton'
 import { MagnifyingGlass } from 'phosphor-react'
-import { useState, useEffect, useRef } from 'react'
-import SearchOverlay from './SearchOverlay'
+import { useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
-import useOnClickOutside from 'use-onclickoutside'
+import { IconButton } from './IconButton'
+import SearchOverlay from './SearchOverlay'
 
 export default function SearchButton() {
   const [active, toggleActive] = useState(false)
-  const clickedOutside = useRef(null)
 
-  // TODO: Poor solution - this logic should be moved into SearchOverlay, but I ran into bugs doing so.
-  // Refactor later to move hotkey and click detection to SearchOverlay.
   useHotkeys('cmd+k', () => toggleActive(true), {
     enableOnTags: ['TEXTAREA', 'INPUT']
   })
-  useHotkeys('esc', () => toggleActive(false), {
-    enableOnTags: ['TEXTAREA', 'INPUT']
-  })
+
+  const updateActive = (state) => {
+    toggleActive(state)
+  }
 
   return (
     <>
@@ -25,7 +22,7 @@ export default function SearchButton() {
         aria-label="Search for content">
         <MagnifyingGlass weight="bold" />
       </IconButton>
-      {active ? <SearchOverlay /> : null}
+      {active ? <SearchOverlay changeActive={updateActive} /> : null}
     </>
   )
 }
