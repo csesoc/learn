e8f7116dc887471e71eb9b348eca160de4b61061
Isaiah Iliffe <isaiahiliffe@gmail.com>
2022-06-15 01:23:25 +1000

    Add mobile responsiveness and fail to fix carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Avatar from 'boring-avatars'
-import { Box } from 'components/Box'
+import { Box, styled } from '@modulz/design-system'
 import { Button } from 'components/Button'
 import { Flex } from 'components/Flex'
 import { Text } from 'components/Text'
@@ -10,7 +10,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import { ArrowRight, CaretCircleLeft, CaretCircleRight } from 'phosphor-react'
+import { ArrowRight, CaretLeft, CaretRight } from 'phosphor-react'
 import { DiscordLogo, FacebookLogo, InstagramLogo } from 'phosphor-react'
 
 import {
@@ -18,9 +18,71 @@ import {
   CarouselSlideList,
   CarouselSlide,
   CarouselNext,
-  CarouselPrevious,
-  CarouselArrowButton,
-} from 'components/Carousel';
+  CarouselPrevious
+} from 'components/Carousel'
+
+const CarouselArrowButton = styled('button', {
+  unset: 'all',
+  outline: 0,
+  margin: 0,
+  border: 0,
+  padding: 0,
+
+  display: 'flex',
+  position: 'relative',
+  zIndex: 1,
+  ai: 'center',
+  jc: 'center',
+  bc: '$panel',
+  br: '$round',
+  width: '$7',
+  height: '$7',
+  color: '$hiContrast',
+
+  boxShadow: '$colors$blackA11 0px 2px 12px -5px, $colors$blackA5 0px 1px 3px',
+  willChange: 'transform, box-shadow, opacity',
+  transition: 'all 100ms',
+
+  '@hover': {
+    '&:hover': {
+      boxShadow:
+        '$colors$blackA10 0px 3px 16px -5px, $colors$blackA5 0px 1px 3px',
+      transform: 'translateY(-1px)',
+
+      // Fix a bug when hovering at button edges would cause the button to jitter because of transform
+      '&::before': {
+        content: '',
+        inset: -2,
+        br: '$round',
+        position: 'absolute'
+      }
+    }
+  },
+  '&:focus': {
+    boxShadow: `
+      $colors$blackA10 0px 3px 16px -5px,
+      $colors$blackA5 0px 1px 3px,
+      $colors$blue8 0 0 0 2px
+    `,
+    transform: 'translateY(-1px)'
+  },
+  '&:focus:not(:focus-visible)': {
+    boxShadow: '$colors$blackA11 0px 2px 12px -5px, $colors$blackA5 0px 1px 3px'
+  },
+  '&:active:not(:focus)': {
+    boxShadow: '$colors$blackA11 0px 2px 12px -5px, $colors$blackA5 0px 1px 3px'
+  },
+  '&:active': {
+    transform: 'none',
+    transition: 'opacity 100ms'
+  },
+  '&:disabled': {
+    opacity: 0
+  },
+  '@media (hover: none) and (pointer: coarse)': {
+    display: 'none'
+  }
+})
 
 export async function getStaticProps() {
   const articles = allArticles.sort((a, b) => {
@@ -31,50 +93,51 @@ export async function getStaticProps() {
 
 function ArticleSlide(article: Article) {
   return (
-<CarouselSlide>
-  <Link href={`/articles/${article.slug}`}>
-    <Card css={{
-      overflow: 'hidden' ,
-      cursor: 'pointer',
-      '&:hover': {
-        backgroundColor: '$slate3'
-      }
-    }}>
-      <Box css={{ margin: '-$6 -$5 $5 -$5' }}>
-        <Image src="/csesoccard2.png" width="700" height="300" />
-      </Box>
-      <Text size="label-md" css={{ color: '$slate11' }}>
-        {format(parseISO(article.date), 'LLL d, yyy')} ⸱{' '}
-        {article.readingTime.text}
-      </Text>
-      <Text
-        size="title-md"
-        css={{ color: '$slate12', fontWeight: '600', paddingTop: '$2' }}>
-        {article.title}
-      </Text>
-      <Text size="label-lg" css={{ color: '$slate12', paddingTop: '$1' }}>
-        {article.desc}
-      </Text>
-      <Flex
-        css={{
-          flexDirection: 'row',
-          gap: '0.75rem',
-          alignItems: 'center',
-          paddingTop: '$5'
-        }}>
-        <Avatar
-          size={28}
-          name={article.author}
-          variant="beam"
-          colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
-        />
-        <Text size="label-md" css={{ color: '$slate12' }}>
-          {article.author}
-        </Text>
-      </Flex>
-    </Card>
-  </Link>
-</CarouselSlide>
+    <CarouselSlide>
+      <Link href={`/articles/${article.slug}`}>
+        <Card
+          css={{
+            overflow: 'hidden',
+            cursor: 'pointer',
+            '&:hover': {
+              backgroundColor: '$slate3'
+            }
+          }}>
+          <Box css={{ margin: '-$6 -$5 $5 -$5' }}>
+            <Image src="/csesoccard2.png" width="700" height="300" />
+          </Box>
+          <Text size="label-md" css={{ color: '$slate11' }}>
+            {format(parseISO(article.date), 'LLL d, yyy')} ⸱{' '}
+            {article.readingTime.text}
+          </Text>
+          <Text
+            size="title-md"
+            css={{ color: '$slate12', fontWeight: '600', paddingTop: '$2' }}>
+            {article.title}
+          </Text>
+          <Text size="label-lg" css={{ color: '$slate12', paddingTop: '$1' }}>
+            {article.desc}
+          </Text>
+          <Flex
+            css={{
+              flexDirection: 'row',
+              gap: '0.75rem',
+              alignItems: 'center',
+              paddingTop: '$5'
+            }}>
+            <Avatar
+              size={28}
+              name={article.author}
+              variant="beam"
+              colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+            />
+            <Text size="label-md" css={{ color: '$slate12' }}>
+              {article.author}
+            </Text>
+          </Flex>
+        </Card>
+      </Link>
+    </CarouselSlide>
   )
 }
 
@@ -118,7 +181,7 @@ const Home: NextPage = ({ articles }: any) => {
         <Text size="headline" css={{ fontWeight: 600, py: '$8' }}>
           See what&apos;s new
         </Text>
-        
+
         <Box css={{ position: 'relative' }}>
           <Carousel>
             <CarouselSlideList
@@ -132,51 +195,56 @@ const Home: NextPage = ({ articles }: any) => {
                 WebkitOverflowScrolling: 'touch',
 
                 // Gap between slides
-                $$gap: '$space$5',
+                $$gap: '$space$6',
 
                 // calculate the left padding to apply to the scrolling list
                 // so that the carousel starts aligned with the container component
                 // the "1145" and "$5" values comes from the <Container /> component
-                '$$scroll-padding': 'max($$gap, calc((100% - 1145px) / 2 + $$gap))',
+                '$$scroll-padding':
+                  'max($$gap, calc((100% - 1145px) / 2 + $$gap))',
                 pl: '$$scroll-padding',
 
                 // hide scrollbar
                 MsOverflowStyle: 'none',
                 scrollbarWidth: 'none',
                 '&::-webkit-scrollbar': {
-                  display: 'none',
+                  display: 'none'
                 },
 
                 // Can't have nice grid gap because Safari butchers scroll padding with it
                 '& > *': {
-                  pr: '$$gap',
-                },
-              }}
-            >
+                  pr: '$$gap'
+                }
+              }}>
               {articles.map((article: Article, index: number) => (
                 <ArticleSlide key={index} {...article} />
               ))}
             </CarouselSlideList>
+
             <Box
               css={{
                 position: 'absolute',
                 top: 'calc(50% - $7)',
-                left: '15px',
-              }}
-            >
-              <CarouselPrevious aria-label="Show previous demo" tabIndex={-1} as={CarouselArrowButton}>
-                <CaretCircleLeft />
+                left: '15px'
+              }}>
+              <CarouselPrevious
+                aria-label="Show previous demo"
+                tabIndex={-1}
+                as={CarouselArrowButton}>
+                <CaretLeft />
               </CarouselPrevious>
             </Box>
             <Box
               css={{
                 position: 'absolute',
                 top: 'calc(50% - $7)',
-                right: '15px',
-              }}
-            >
-              <CarouselNext aria-label="Show next demo" tabIndex={-1} as={CarouselArrowButton}>
-                <CaretCircleRight />
+                right: '15px'
+              }}>
+              <CarouselNext
+                aria-label="Show next demo"
+                tabIndex={-1}
+                as={CarouselArrowButton}>
+                <CaretRight />
               </CarouselNext>
             </Box>
           </Carousel>
@@ -196,7 +264,9 @@ const Home: NextPage = ({ articles }: any) => {
           css={{
             px: '$6',
             paddingTop: '$8',
-            gap: '$7'
+            gap: '$7',
+            flexWrap: 'wrap',
+            justifyContent: 'center'
           }}>
           <Card css={{ backgroundColor: '$slate3', overflow: 'hidden' }}>
             {/* slateDark3 doesn't work */}
@@ -222,7 +292,7 @@ const Home: NextPage = ({ articles }: any) => {
                   marginTop: '$6',
                   cursor: 'pointer',
                   backgroundColor: '$green6',
-                  '&:hover': { backgroundColor: '$green7' },
+                  '&:hover': { backgroundColor: '$green7' }
                 }}>
                 Contribute
                 <ArrowRight weight="bold" />
