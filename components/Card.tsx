eec7ff7ca9cc585324349c186b1a76f8c5ab90de
Max Phillips <max.phillips@nine.com.au>
2022-06-26 20:52:13 +1000

    extra test data

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,9 +3,9 @@ import { styled } from 'stitches.config'
 export const Card = styled('div', {
   display: 'flex',
   flexDirection: 'column',
-  padding: '$6 $5',
-  width: '22rem',
-  height: '25rem',
+  minWidth: '20rem',
+  cursor: 'pointer',
+  height: '25em',
   boxShadow: '$default',
   background: '$slate1',
   borderRadius: '$lg'
