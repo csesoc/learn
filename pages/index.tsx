344f9bad1bb384e9460aab7755e9e6c045e81361
Max Phillips <max.phillips@nine.com.au>
2022-06-26 21:57:14 +1000

    add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -188,6 +188,26 @@ const Home: NextPage<Props> = ({ articles }) => {
             </Flex>
           </Flex>
         </Flex>
+        <Flex
+          as="footer"
+          direction="column"
+          justify="center"
+          align="center"
+          css={{ width: '100%', marginTop: '$8', marginBottom: '$6' }}>
+          <Text
+            css={{
+              color: '$slate10',
+              marginBottom: '$4'
+            }}>
+            MIT Licensed • Made with ❤ by CSESoc Education
+          </Text>
+          <Text
+            css={{
+              color: '$slate10'
+            }}>
+            {new Date().getFullYear()}, Sydney
+          </Text>
+        </Flex>
       </Flex>
     </Box>
   )
