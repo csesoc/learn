fea4c42f291a8ebddc95e253e5f495abb04212c8
Gordon Huang <dqna64@gmail.com>
2022-06-27 18:03:18 +1000

    remove duplicate files

--- components/Box.tsx
+++
@@ -1,8 +0,0 @@
-// https://github.com/radix-ui/design-system/blob/master/components/Box.tsx
-
-import { styled } from 'stitches.config'
-
-export const Box = styled('div', {
-  // Reset
-  boxSizing: 'border-box'
-})
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-import { withContentlayer } from 'next-contentlayer'
-
-const nextConfig = {
-  swcMinify: true,
-  reactStrictMode: true
-}
-
-export default withContentlayer({ nextConfig })
