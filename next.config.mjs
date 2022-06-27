eedd587e439292e46c7b534b0d1be79c665fda03
Gordon Huang <dqna64@gmail.com>
2022-06-28 01:39:28 +1000

    add tuts 2,3, add support for images in mdx, create centerer component to center mdx images

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ import { withContentlayer } from 'next-contentlayer'
 
 const nextConfig = {
   swcMinify: true,
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    domains: ['imgur.com']
+  }
 }
 
 export default withContentlayer({ nextConfig })
