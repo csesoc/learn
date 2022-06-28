be0b8716a362858522d4c08937633a3653f4f92c
Max Phillips <max.phillips@nine.com.au>
2022-06-28 12:15:36 +1000

    add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Partytown } from '@builder.io/partytown/react'
 
-export default function Document () {
+export default function Document() {
   return (
     <Html>
       <Head>
@@ -8,6 +9,33 @@ export default function Document () {
           rel="stylesheet"
           href="https://files.stork-search.net/releases/v1.4.2/basic.css"
         />
+        <Partytown
+          debug={process.env.NODE_ENV !== 'production'}
+          forward={['dataLayer.push']}
+        />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <script
+              type="text/partytown"
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+            <script
+              type="text/partytown"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `
+              }}
+            />
+          </>
+        )}
       </Head>
       <body>
         <Main />
