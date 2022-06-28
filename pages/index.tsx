435bc5d839ef085828433e68d92b52c4d4d8aab0
Isaiah Iliffe <isaiahiliffe@gmail.com>
2022-06-28 17:43:57 +1000

    Fix carousel and other miscellaneous layout tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,109 +1,23 @@
 import { Box } from 'components/Box'
 import { Button } from 'components/Button'
+import { Card } from 'components/Card'
 import { Flex } from 'components/Flex'
 import { Text } from 'components/Text'
-import { Card } from 'components/Card'
-import { styled } from '../stitches.config'
-import { allArticles, Article } from 'contentlayer/generated'
-import { compareDesc, format, parseISO } from 'date-fns'
+import { allArticles } from 'contentlayer/generated'
+import { compareDesc } from 'date-fns'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import {
-  ArrowRight,
-  CaretLeft,
-  CaretRight,
-  DiscordLogo,
-  FacebookLogo,
-  InstagramLogo
-} from 'phosphor-react'
-import ArticleCard from 'components/ArticleCard'
-
-import {
-  Carousel,
-  CarouselSlideList,
-  CarouselSlide,
-  CarouselNext,
-  CarouselPrevious
-} from 'components/Carousel'
-
-const CarouselArrowButton = styled('button', {
-  unset: 'all',
-  outline: 0,
-  margin: 0,
-  border: 0,
-  padding: 0,
-
-  display: 'flex',
-  position: 'relative',
-  zIndex: 1,
-  ai: 'center',
-  jc: 'center',
-  bc: '$panel',
-  br: '$round',
-  width: '$7',
-  height: '$7',
-  color: '$hiContrast',
-
-  boxShadow: '$colors$blackA11 0px 2px 12px -5px, $colors$blackA5 0px 1px 3px',
-  willChange: 'transform, box-shadow, opacity',
-  transition: 'all 100ms',
-
-  '@hover': {
-    '&:hover': {
-      boxShadow:
-        '$colors$blackA10 0px 3px 16px -5px, $colors$blackA5 0px 1px 3px',
-      transform: 'translateY(-1px)',
-
-      // Fix a bug when hovering at button edges would cause the button to jitter because of transform
-      '&::before': {
-        content: '',
-        inset: -2,
-        br: '$round',
-        position: 'absolute'
-      }
-    }
-  },
-  '&:focus': {
-    boxShadow: `
-      $colors$blackA10 0px 3px 16px -5px,
-      $colors$blackA5 0px 1px 3px,
-      $colors$blue8 0 0 0 2px
-    `,
-    transform: 'translateY(-1px)'
-  },
-  '&:focus:not(:focus-visible)': {
-    boxShadow: '$colors$blackA11 0px 2px 12px -5px, $colors$blackA5 0px 1px 3px'
-  },
-  '&:active:not(:focus)': {
-    boxShadow: '$colors$blackA11 0px 2px 12px -5px, $colors$blackA5 0px 1px 3px'
-  },
-  '&:active': {
-    transform: 'none',
-    transition: 'opacity 100ms'
-  },
-  '&:disabled': {
-    opacity: 0
-  },
-  '@media (hover: none) and (pointer: coarse)': {
-    display: 'none'
-  }
-})
+import { ArrowRight } from 'phosphor-react'
+import { DiscordLogo, FacebookLogo, InstagramLogo } from 'phosphor-react'
+import { ArticlesCarousel } from 'components/ArticlesCarousel'
 
 export async function getStaticProps() {
   const articles = allArticles.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date))
+    return compareDesc(new Date(b.date), new Date(a.date))
   })
   return { props: { articles } }
-}
-
-function ArticleSlide(article: Article) {
-  return (
-    <CarouselSlide>
-      <ArticleCard article={article} />
-    </CarouselSlide>
-  )
 }
 
 const Home: NextPage = ({ articles }: any) => {
@@ -117,7 +31,6 @@ const Home: NextPage = ({ articles }: any) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Flex
         as="main"
         css={{
@@ -143,78 +56,19 @@ const Home: NextPage = ({ articles }: any) => {
             <ArrowRight weight="bold" />
           </Button>
         </Link>
-        <Text size="headline" css={{ fontWeight: 600, py: '$8' }}>
+        <Text
+          size="headline"
+          css={{ fontWeight: 600, paddingTop: '$8', paddingBottom: '$4' }}>
           See what&apos;s new
         </Text>
-
-        <Box css={{ position: 'relative' }}>
-          <Carousel>
-            <CarouselSlideList
-              css={{
-                display: 'grid',
-                gridAutoFlow: 'column',
-                gridAutoColumns: 'min-content',
-                ox: 'auto',
-                oy: 'hidden',
-                py: '$1',
-                WebkitOverflowScrolling: 'touch',
-
-                // Gap between slides
-                $$gap: '$space$6',
-
-                // calculate the left padding to apply to the scrolling list
-                // so that the carousel starts aligned with the container component
-                // the "1145" and "$5" values comes from the <Container /> component
-                '$$scroll-padding':
-                  'max($$gap, calc((100% - 1145px) / 2 + $$gap))',
-                pl: '$$scroll-padding',
-
-                // hide scrollbar
-                MsOverflowStyle: 'none',
-                scrollbarWidth: 'none',
-                '&::-webkit-scrollbar': {
-                  display: 'none'
-                },
-
-                // Can't have nice grid gap because Safari butchers scroll padding with it
-                '& > *': {
-                  pr: '$$gap'
-                }
-              }}>
-              {articles.map((article: Article, index: number) => (
-                <ArticleSlide key={index} {...article} />
-              ))}
-            </CarouselSlideList>
-
-            <Box
-              css={{
-                position: 'absolute',
-                top: 'calc(50% - $7)',
-                left: '15px'
-              }}>
-              <CarouselPrevious
-                aria-label="Show previous demo"
-                tabIndex={-1}
-                as={CarouselArrowButton}>
-                <CaretLeft />
-              </CarouselPrevious>
-            </Box>
-            <Box
-              css={{
-                position: 'absolute',
-                top: 'calc(50% - $7)',
-                right: '15px'
-              }}>
-              <CarouselNext
-                aria-label="Show next demo"
-                tabIndex={-1}
-                as={CarouselArrowButton}>
-                <CaretRight />
-              </CarouselNext>
-            </Box>
-          </Carousel>
-        </Box>
-
+      </Flex>
+      <ArticlesCarousel articles={articles} />
+      <Flex
+        as="main"
+        css={{
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
         <Text size="headline" css={{ fontWeight: 600, paddingTop: '$8' }}>
           Join the CSESoc community
         </Text>
@@ -228,7 +82,7 @@ const Home: NextPage = ({ articles }: any) => {
         <Flex
           css={{
             px: '$6',
-            paddingTop: '$8',
+            paddingTop: '$6',
             gap: '$7',
             flexWrap: 'wrap',
             justifyContent: 'center'
@@ -270,13 +124,16 @@ const Home: NextPage = ({ articles }: any) => {
             direction="column"
             justify="between"
             align="center"
-            css={{ height: '25rem' }}>
+            css={{ height: '28rem' }}>
             {/* TODO: avoid copypaste for social media buttons */}
-            <a href="https://cseso.cc/discord/" target="blank">
+            <a
+              href="https://cseso.cc/discord/"
+              target="blank"
+              style={{ textDecoration: 'none' }}>
               <Button
-                size="default"
+                size="large"
                 css={{
-                  width: '22rem',
+                  width: '24em',
                   cursor: 'pointer',
                   backgroundColor: '$indigo4',
                   '&:hover': { backgroundColor: '$indigo5' }
@@ -294,11 +151,14 @@ const Home: NextPage = ({ articles }: any) => {
                 </Flex>
               </Button>
             </a>
-            <a href="https://www.facebook.com/csesoc/" target="blank">
+            <a
+              href="https://www.facebook.com/csesoc/"
+              target="blank"
+              style={{ textDecoration: 'none' }}>
               <Button
-                size="default"
+                size="large"
                 css={{
-                  width: '22rem',
+                  width: '24em',
                   marginTop: '$6',
                   cursor: 'pointer',
                   backgroundColor: '$blue4',
@@ -316,11 +176,14 @@ const Home: NextPage = ({ articles }: any) => {
                 </Flex>
               </Button>
             </a>
-            <a href="https://www.instagram.com/csesoc_unsw/" target="blank">
+            <a
+              href="https://www.instagram.com/csesoc_unsw/"
+              target="blank"
+              style={{ textDecoration: 'none' }}>
               <Button
-                size="default"
+                size="large"
                 css={{
-                  width: '22rem',
+                  width: '24em',
                   marginTop: '$6',
                   cursor: 'pointer',
                   backgroundColor: '$pink4',
