fd8dd1f79e3524608bda25e7440f022ec2aa5d5d
Laute <52260930+Laute@users.noreply.github.com>
2022-07-30 15:23:13 +1000

    Implement MultiChoice component (#95)
    * Implement MultiChoice component
    
    * Update explanations for long ones to be expandable
    
    * Clean up code
    
    * Update previewText logic
    
    * Add default value for isCorrect prop
    
    * Add content prop for short answers/explanations
    
    * Change filter to find
    
    * Change long explanation threshold
    
    * Change long explanation threshold
    
    * Refactor subcomponents into their own modules
    
    * Refactor subcomponents into their own modules
    
    * Rename children array variables
    
    * ignore ts warnings in ArticleCarousel
    
    * add MultiChoice.Explanation preview support for nested components
    
    * fix misc ts errors
    
    * Add comp2521 revision theory page
    
    Co-authored-by: Gordon Huang <dqna64@gmail.com>
    Co-authored-by: jasperdifran <68628968+jasperdifran@users.noreply.github.com>

---
+++ components/MultiChoice.tsx
@@ -0,0 +1,43 @@
+import { Children, cloneElement, ReactElement, ReactNode } from 'react'
+import { styled } from '../stitches.config'
+import { Flex } from './Flex'
+import Answer from './MultiChoiceAnswer'
+import Explanation from './MultiChoiceExplanation'
+import Question from './MultiChoiceQuestion'
+
+const MultiChoiceBase = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: '$slate1',
+  padding: '0 $3 $3',
+  borderRadius: '8px',
+  margin: '$3 0 $3'
+})
+
+interface MultiChoiceProps {
+  children: ReactNode
+}
+
+const MultiChoice = ({ children }: MultiChoiceProps) => {
+  const childrenArray = Children.toArray(children) as ReactElement[]
+  const question = childrenArray.find(
+    (child) => child.type === MultiChoice.Question
+  )
+  const answers = childrenArray
+    .filter((child) => child.type === MultiChoice.Answer)
+    // assign answer a number based on order, shown as a number circle
+    .map((answer, index) => cloneElement(answer, { answerNum: index + 1 }))
+
+  return (
+    <MultiChoiceBase>
+      {question}
+      <Flex direction="column">{answers}</Flex>
+    </MultiChoiceBase>
+  )
+}
+
+MultiChoice.Question = Question
+MultiChoice.Answer = Answer
+MultiChoice.Explanation = Explanation
+
+export default MultiChoice
---
+++ components/MultiChoiceExplanation.tsx
@@ -0,0 +1,92 @@
+import React, { Children, ReactElement, ReactNode, useState } from 'react'
+import { callbackify } from 'util'
+import { styled } from '../stitches.config'
+
+const ExplanationBase = styled('div', {
+  color: 'grey'
+})
+
+type ExplanationProps = {
+  content: ReactElement
+  children: JSX.Element[]
+}
+
+const Explanation = ({ content, children }: ExplanationProps) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  // content is used for short explanations
+  // long explanations use the children prop
+  if (content) {
+    return (
+      <ExplanationBase>
+        <p>{content}</p>
+      </ExplanationBase>
+    )
+  }
+
+  console.log("children", children)
+    const traverse = (ele: JSX.Element[] | JSX.Element | string, callback: (ele: string) => void) => {
+    if (typeof ele === 'string') {
+      // console.log("ele is string:", ele)
+      callback(ele);
+      return;
+    }
+    if (Array.isArray(ele)) {
+      // console.log("ele is array:", ele)
+      ele.forEach((subEle) => traverse(subEle, callback));
+    } else if (React.isValidElement(ele)) {
+      // console.log("ele is react element:", ele)
+      if (ele.props.hasOwnProperty('children')) {
+        // Only if the props of this react element has a children prop.
+        // Might not in some cases, e.g. when ele.type === "img"
+        traverse((ele as JSX.Element).props.children, callback);
+      }
+    }
+  }
+
+  // Traverse the component tree to count total length of text.
+  let textLength = 0;
+  traverse(children, (text) => {
+    textLength += text.length
+  })
+  
+  const handleOnClick = (event) => {
+    // prevents collapsing the parent component
+    event.stopPropagation()
+    setIsExpanded((prev) => !prev)
+  }
+
+  // above collapsable threshold
+  const threshold = 500
+  if ((children.length > 1 || textLength > threshold) && !isExpanded) {
+    let previewTextFound = false
+    let previewText = ""
+
+    // Traverse the component tree and find the first text node.    
+    traverse(children, (text: string) => {
+      if (!previewTextFound) {
+        previewText = text
+        previewTextFound = true
+      }
+    })
+
+    previewText.substring(0, threshold)
+      // trims all non-letter characters from end of string
+      // this ensures the truncated paragraph looks good with the ellipsis (...)
+      .replace(/[^a-z]+$/gi, '')
+
+    return (
+      <ExplanationBase>
+        <p>
+          {previewText}
+          {'... '}
+          <a onClick={(event) => handleOnClick(event)}>Read more</a>
+        </p>
+      </ExplanationBase>
+    )
+  }
+
+  return <ExplanationBase>{children}</ExplanationBase>
+}
+
+export default Explanation
