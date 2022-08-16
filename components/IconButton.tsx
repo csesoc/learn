d93d9512f00d21429884545b3ccf84ea583f5f7e
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-08-16 14:39:04 +1000

    Fixed issues in #24

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -8,13 +8,15 @@ export const IconButton = styled('button', {
   backgroundColor: '$slate4',
   padding: '10px 10px 10px 10px',
   width: 'max-content',
+  height: 'max-content',
   borderRadius: '$full',
   cursor: 'pointer',
   transition: '$button',
   border: 'none',
   '& svg': {
     fill: '$slate12',
-    width: '20px'
+    width: '20px',
+    height: '20px'
   },
   '&:hover': {
     backgroundColor: '$slate5'
