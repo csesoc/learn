8e62dce3905c5e9902dc6b30b22ba520db20ae8c
Max Phillips <max@maxphillips.dev>
2023-03-28 01:14:50 +1100

    setup sitemap

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
